358796ac4f3070043db0a4ce4fea090274b0d671
waizin28 <waizinlinn.08@gmail.com>
2024-11-07 17:55:46 -0600

    added snack bar notification when painting added to cart

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from 'src/app/models/product';
-
+import { CartService } from 'src/app/cart/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -10,11 +11,28 @@ import { Product } from 'src/app/models/product';
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data) => {
       this.products = data;
+    });
+  }
+
+  addToCart(product: Product): void {
+    this.cartService.addToCart(product).subscribe({
+      // After add to cart successful, want to show snackbar
+      next: () => {
+        this.snackBar.open('Product added to cart!', '', {
+          duration: 2000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        });
+      },
     });
   }
 }
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { MatCardModule } from '@angular/material/card';
 import { FlexModule } from '@angular/flex-layout';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 @NgModule({
   declarations: [ProductListComponent],
-  imports: [CommonModule, MatCardModule, FlexModule],
+  imports: [CommonModule, MatCardModule, FlexModule, MatSnackBarModule],
 })
 export class ProductModule {}
